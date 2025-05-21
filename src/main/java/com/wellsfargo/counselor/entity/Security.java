aea826118c0427b3de0f5bb5378a256cac255a83
Vandana1603 <vandanakbng@gmail.com>
2025-05-21 16:43:28 +0530

    Create Security.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,43 @@
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private LocalDate purchaseDate;
+
+    @Column(nullable = false)
+    private Double purchasePrice;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private Portfolio portfolio;
+
+    protected Security() {}
+
+    public Security(String name, String category, LocalDate purchaseDate, Double purchasePrice, Integer quantity, Portfolio portfolio) {
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.portfolio = portfolio;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    // Getters and Setters
+}
