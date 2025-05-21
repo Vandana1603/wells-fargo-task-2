2157a3bc04f193f5bd83ad21d2410146e51a859c
Vandana1603 <vandanakbng@gmail.com>
2025-05-21 16:30:17 +0530

    Create Advisornew.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Advisornew.java
@@ -0,0 +1,38 @@
+@Entity
+public class Advisornew {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    protected Advisor() {}
+
+    public Advisor(String firstName, String lastName, String address, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+    
+    // Getters and Setters
+}
